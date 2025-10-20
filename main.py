1461109c7a0a8eefed6dccb860a0901d2fc2b261
Dhiraj Vats <dhiraj.cybnetic@gmail.com>
2025-10-20 10:45:09 +0530

    init commit

---
+++ main.py
@@ -0,0 +1,13 @@
+# Sample array
+numbers = [5, 10, 15, 20, 25]
+
+# Initialize sum variable
+total = 0
+
+# Loop through the array and print each element
+for num in numbers:
+    print("Element:", num)
+    total += num  # Add to total
+
+# Print total sum
+print("Total sum of array:", total)
